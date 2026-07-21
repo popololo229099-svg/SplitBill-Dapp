d61ce443b5e22f817dae013112d5ccaa876e6692
popololo229099-svg <popololo229099@gmail.com>
2026-07-21 15:13:03 +0530

    Add Vercel frontend URL to CORS origins

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:4173'] });
+  app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:4173', 'https://xlm-payment-dapp.vercel.app'] });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
